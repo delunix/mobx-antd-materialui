ad151a37054f20f53b492389168bc4c954f1f7f2
Fandi Rahmawan <rahmawan.fandi@gmail.com>
2019-10-29 08:40:12 +0700

    Updates

---
+++ precache-manifest.59a86d851f311f332050924b0baa76ab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8ad203f23ee20ffda0e5c92a58e943",
+    "url": "/mobx-antd-materialui/index.html"
+  },
+  {
+    "revision": "4daf7ab6e5b679502417",
+    "url": "/mobx-antd-materialui/static/css/2.0afd3a24.chunk.css"
+  },
+  {
+    "revision": "d493b95c1b92baad8d17",
+    "url": "/mobx-antd-materialui/static/css/main.dff7aee2.chunk.css"
+  },
+  {
+    "revision": "4daf7ab6e5b679502417",
+    "url": "/mobx-antd-materialui/static/js/2.43fad028.chunk.js"
+  },
+  {
+    "revision": "d493b95c1b92baad8d17",
+    "url": "/mobx-antd-materialui/static/js/main.d1624c46.chunk.js"
+  },
+  {
+    "revision": "dccb2b3d599ea60500de",
+    "url": "/mobx-antd-materialui/static/js/runtime-main.6ee2fd36.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/mobx-antd-materialui/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "99fd3c1bce21608718d0848d2c81187c",
+    "url": "/mobx-antd-materialui/static/media/the-hari-logo.99fd3c1b.png"
+  },
+  {
+    "revision": "9d4bc7b7e5caa2b2112cd9a58bc22687",
+    "url": "/mobx-antd-materialui/static/media/to-do-list.9d4bc7b7.png"
+  }
+]);
